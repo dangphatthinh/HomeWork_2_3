5168f6ec615b7f5612c0829e788e2030c20c5993
May10 <May10@MAY10>
2022-06-19 14:18:41 +0700

    Add project files.

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class Program
+{   
+    public static void Main()
+    {
+        Board b = new Board();
+        Data d = new Data();
+        b.Size = int.Parse(Console.ReadLine());
+        d.ResetArr(d.arr, b.Size);
+        b.DrawBoard(d.arr,b.Size);
+    }
+}
+class Data
+{
+    public int[,] arr = new int[100, 100];
+    public void ResetArr(int[,] arr,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                arr[i,j] = 32;
+            }           
+        }
+    }
+}
+class Board
+{
+    private int size;
+    public int Size { 
+        get { return size; }
+        set { size = value; }
+    }
+    
+    public void DrawBoard(int[,] arr,int size)
+    {
+        for(int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write("   {0}   ", (char)arr[i,j]);
+                if(j == size - 1)
+                {
+                    break;
+                }
+                Console.Write("|");
+            }
+            Console.WriteLine();
+            if (i == size - 1)
+            {
+                break;
+            }
+            for (int k = 0; k < size; k++)
+            {
+                Console.Write("-------");
+                if (k == size - 1)
+                {
+                    break;
+                }
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }      
+    }
+}
